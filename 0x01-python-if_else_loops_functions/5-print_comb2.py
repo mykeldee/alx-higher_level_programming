dfa79e0edee20abe9438c4e1a12685f8a7d54566
Michael Dankyira <kdankyira.education@gmail.com>
2022-08-23 11:12:52 -0700

    Update using only 1 for loop

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(10):
-        if i == 9 and j == 9:
-            print("{}{}".format(i,j))
-        else:
-            print(f"{i}{j}", end=" ")
+for i in range(100):
+    if i == 99:
+        print(f"{i:02d}")
+    else:
+        print(f"{i:02d}", end = " ")
