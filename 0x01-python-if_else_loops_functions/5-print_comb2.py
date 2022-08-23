1e4c90ba460327d1ae8152711b0f194bcd777fba
Michael Dankyira <kdankyira.education@gmail.com>
2022-08-23 11:04:00 -0700

    Update with .format()

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,6 @@
 for i in range(10):
     for j in range(10):
         if i == 9 and j == 9:
-            print(f"{i}{j}")
+            print("{}{}".format(i,j))
         else:
             print(f"{i}{j}", end=" ")
