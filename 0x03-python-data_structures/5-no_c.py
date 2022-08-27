b8eaeec92185f2d92279c28ebdd8753e7dbee385
Michael Dankyira <kdankyira.education@gmail.com>
2022-08-27 14:59:47 -0700

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,7 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 def no_c(my_string):
-    if 'c' in my_string or 'C' in my_string:
-        n = ""
-        for i in range(len(my_string)):
-            if my_string[i] == 'c' or my_string[i] == 'C':
-                continue
-            n += my_string[i]
-        return n
+    a = "cC"
+    b = "  "
+    c = "cC"
+    translation = my_string.maketrans(a, b, c)
+    return my_string.translate(translation)
