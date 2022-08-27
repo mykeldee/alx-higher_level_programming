c74ffb2325e252f992d2bfd1207d7599f8a2f4c2
Michael Dankyira <kdankyira.education@gmail.com>
2022-08-27 13:21:47 -0700

    Update 2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,7 +2,7 @@
 def replace_in_list(my_list, idx, element):
     n = len(my_list)
     if idx < 0:
-        return None
+        return my_list
     if idx >= n:
         return my_list
     my_list[idx] = element
