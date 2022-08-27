9cfd4768328d208373811eaefec4d571a4527b15
Michael Dankyira <kdankyira.education@gmail.com>
2022-08-27 16:31:17 -0700

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,5 @@ def print_matrix_integer(matrix=[[]]):
         for j in range(len(matrix[i])):
             if j > 0:
                 print(" ", end=" ")
-            print(matrix[i][j], end=" ")
+            print(matrix[i][j], end="")
         print()
