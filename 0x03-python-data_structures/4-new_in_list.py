ee34ffeb86de62c31161b5250ceaef76d0de5288
Michael Dankyira <kdankyira.education@gmail.com>
2022-08-27 14:10:43 -0700

    Add 4-new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    n = len(my_list)
+    if idx < 0:
+        return my_list
+    if idx >= n:
+        return my_list
+    new_list = my_list[:]
+    new_list[idx] = element
+    return new_list
