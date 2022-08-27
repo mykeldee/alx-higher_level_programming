f9289b8de4ed65f49352c8a47dbb64bc8d3c7e36
Michael Dankyira <kdankyira.education@gmail.com>
2022-08-27 13:33:11 -0700

    Update 3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
-    n = my_list.sort()
+def print_reversed_list_integer(my_list=[]):    
     n = my_list.reverse()
     x = len(my_list)
     if x > 0:
