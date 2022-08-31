9b0e0247c5d7670da50da4f074960f5588e868e3
Michael Dankyira <kdankyira.education@gmail.com>
2022-08-31 15:01:14 -0700

    Update to return `key` not `value`

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,4 +3,7 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     else:
-        return max(a_dictionary.values())
+        max_val = max(a_dictionary.values())
+        for k, v in a_dictionary.items():
+            if v == max_val:
+                return k
