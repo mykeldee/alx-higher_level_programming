468c4d4b1343d7cfce6f7d3e588411a19b8e440f
Michael Dankyira <kdankyira.education@gmail.com>
2022-09-13 19:47:39 -0700

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 class Square:
-    """type class square"""
+    """Class Square that defines a square object
+    """
     def __init__(self, size):
-        """Init the square class
+        """Initialize method that stores the size of the square
         Args:
-        param1: size is the type int attribute to make it private
+            param1 (int): size of the square
         """
         self.__size = size
